1ccde8bff49b903dbf86d808385600e2667c42bd
Akash Kushwaha <kushwahaakash2000@gmail.com>
2025-02-22 14:59:40 +0530

    changes done

--- src/app/_sections/header.js
+++ src/app/_sections/header.js
@@ -19,35 +19,61 @@ export default function Header() {
                 PROJECT
               </h1>
               <p className="text-lg xl:text-2xl md:text-xl text-slate-600 mt-[3rem] md:mt-6">
-                Tailored Diet Plans, Professional Advice, and Trusted Health Content for Your Wellness Journey
+                Tailored Diet Plans, Professional Advice, and Trusted Health
+                Content for Your Wellness Journey
               </p>
               <div className="lg:flex items-center mt-4 md:mt-6">
                 <div className="flex justify-center space-x-2 pb-4 lg:pb-0">
                   {/* Placeholder for profile images */}
-                  <img src="pic1.png" alt="profile1" className="w-10 h-10 rounded-full border-2 border-white" />
-                  <img src="pic2.png" alt="profile2" className="w-10 h-10 rounded-full border-2 border-white" />
-                  <img src="pic3.png" alt="profile3" className="w-10 h-10 rounded-full border-2 border-white" />
-                  <img src="pic4.png" alt="profile4" className="w-10 h-10 rounded-full border-2 border-white" />
-                  <img src="pic5.png" alt="profile4" className="w-10 h-10 rounded-full border-2 border-white" />
+                  <img
+                    src="Rahul.png"
+                    alt="profile1"
+                    className="w-10 h-10 rounded-full border-2 border-white"
+                  />
+                  <img
+                    src="Shid.png"
+                    alt="profile2"
+                    className="w-10 h-10 rounded-full border-2 border-white"
+                  />
+                  <img
+                    src="sonali.png"
+                    alt="profile3"
+                    className="w-10 h-10 rounded-full border-2 border-white"
+                  />
+                  <img
+                    src="men.jpg"
+                    alt="profile4"
+                    className="w-10 h-10 rounded-full border-2 border-white"
+                  />
+                  <img
+                    src="saily.jpg"
+                    alt="profile4"
+                    className="w-10 h-10 rounded-full border-2 border-white"
+                  />
                 </div>
-                <p className="text-orange-500 text-lg ml-4">5.0 <span className="text-slate-500">From 50+ Success Stories</span></p>
+                <p className="text-orange-500 text-lg ml-4">
+                  5.0{" "}
+                  <span className="text-slate-500">
+                    From 50+ Success Stories
+                  </span>
+                </p>
               </div>
               <div className="lg:flex items-center mt-4 justify-center lg:justify-start md:mt-8">
                 <Link href="/contactform">
-                <button className="bg-orange-600 hover:bg-orange-500 text-white font-bold py-2 mb-4 xl:mb-0 md:py-3 px-4 md:px-8 rounded-lg">
-                  Join Now
-                </button>
+                  <button className="bg-orange-600 hover:bg-orange-500 text-white font-bold py-2 mb-4 xl:mb-0 md:py-3 px-4 md:px-8 rounded-lg">
+                    Join Now
+                  </button>
                 </Link>
-                <p className="ml-2 text-lg font-semibold text-slate-800">30-DAYS MONEY BACK GUARANTEE</p>
+                <p className="ml-2 text-lg font-semibold text-slate-800">
+                  30-DAYS MONEY BACK GUARANTEE
+                </p>
               </div>
-
             </div>
 
             {/* Right Image */}
             <div className="col-span-1">
               <img src="/lady.avif" alt="Meditation" className="mr-4" />
             </div>
-
           </div>
         </section>
       </div>
