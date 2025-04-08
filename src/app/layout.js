3ce74533ba40f6443caca6e155b2fa42819452e3
Nandini <Nandini Vaish>
2025-04-08 20:14:28 +0530

    adds

--- src/app/layout.js
+++ src/app/layout.js
@@ -46,6 +46,21 @@ export default function RootLayout({ children }) {
           />
         </noscript>
 
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+              c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+              t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+              y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+             })(window, document, "clarity", "script", "r0rbj9sv57");
+             `
+          }}
+        />
+
+
         {/* <Navbar /> */}
         <div className="mx-auto max-v-xl text-2xl gap-2">
           {children}
